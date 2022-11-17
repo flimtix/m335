83feb056b4885649e4a395e2621a6581cf7136bb
21r8390 <76058025+21r8390@users.noreply.github.com>
2022-11-17 23:46:09 +0100

    Update removed DB Link

--- MemeChat/MemeChat/MemeChat/Constants.cs
+++ MemeChat/MemeChat/MemeChat/Constants.cs
@@ -3,9 +3,7 @@
     public static class Constants
     {
         public static string LocalDatabasePath => "Data Source=" + Path.Combine(FileSystem.AppDataDirectory, "MemeChatCache.db3");
-
-        public const string ServerDbConnectionString = "Data Source=153.92.6.106;Port=3306;Initial Catalog=u166616309_MemeChat;User ID=u166616309_MemeChatUser;Password=1b&mhtTNELnA";
-
+        public const string ServerDbConnectionString = "Data Source";
         public const string SecureStorage_Nickname = "nickname";
     }
 }
